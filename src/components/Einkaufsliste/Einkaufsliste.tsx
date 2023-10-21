2fec9769308af247f9a21fabbcafc9deb48ebb0c
Floatyy1998 <you@example.com>
2023-10-21 15:36:31 +0200

    changed destructiveCallbackDelay

--- src/components/Einkaufsliste/Einkaufsliste.tsx
+++ src/components/Einkaufsliste/Einkaufsliste.tsx
@@ -132,7 +132,7 @@ const Einkaufsliste = () => {
               </Button>
             </div>
           </form>
-          <SwipeableList threshold={0.2} destructiveCallbackDelay={400}>
+          <SwipeableList threshold={0.2} destructiveCallbackDelay={300}>
             {itemList
               .sort((a, b) => {
                 if (a.name.toUpperCase() < b.name.toUpperCase()) {
